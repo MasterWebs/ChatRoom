2dfeacf711955afe778850156c11002e6b09ff0b
Snaebjorn <snaebjorn13@ru.is>
2015-02-11 13:30:49 +0000

    added module and logincontroller to app.js, also added chatserver.js file

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var ChatRoom = angular.module('ChatRoom', ['ngRoute']);
+
+ChatRoom.config(
+	function ($routeProvider) {
+		$routeProvider
+		.when('login', { templateUrl: 'views/login.html', controller: 'LoginController' });
+	}
+);
+
+ChatRoom.controller('LoginController', function($scope, $location, $rootScope, $routeParams, socket) {
+
+	$scope.errorMessage = '';
+	$scope.nickname = '';
+
+	$scope.login = function() {
+		if ($scope.nickname = '') {
+			$scope.errorMessage = 'Please choose a nickname before continuing!';
+		} else {
+			socket.emit('addUser', $scope.nickname, function (available) {
+				if (available) {
+					$location.path('/rooms/' + $scope.nickname);
+				} else {
+					$scope.errorMessage = 'This nickname is already taken!';
+				}
+			});
+		}
+	};
+});
