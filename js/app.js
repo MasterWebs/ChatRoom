174393b40bcae3d021e9b88d34ad854785963770
omaroskars <omar13@ru.is>
2015-02-14 19:18:20 +0000

    .

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ ChatRoom.config(
 				templateUrl: 'views/login.html', 
 				controller: 'LoginController' 
 			})
+			.when('/rooms', {
+				templateUrl: 'views/rooms.html',
+				controller: 'RoomsController'
+			})
 			.otherwise({
 				redirectTo: '/login'
 			});
@@ -19,10 +23,13 @@ ChatRoom.controller('LoginController', function($scope, $location, $rootScope, $
 	$scope.nickname = '';
 
 	$scope.login = function() {
+		console.log("login");
 		if ($scope.nickname = '') {
+			console.log("error, empty nick");
 			$scope.errorMessage = 'Please choose a nickname before continuing!';
 		} else {
 			socket.emit('addUser', $scope.nickname, function (available) {
+				console.log("socker");
 				if (available) {
 					$location.path('/rooms/' + $scope.nickname);
 				} else {
@@ -31,4 +38,22 @@ ChatRoom.controller('LoginController', function($scope, $location, $rootScope, $
 			});
 		}
 	};
+});
+
+ChatRoom.controller('RoomsController', function($scope, $location, $rootScope, $routeParams, socket) {
+	$scope.rooms = ['Room1', 'Room2', 'Room3'];
+	$scope.currentUser = $routeParams.user;
+});
+
+ChatRoom.controller('RoomController', function($scope, $location, $rootScope, $routeParams, socket) {
+	$scope.currentRoom = $routeParams.room;
+	$scope.currentUser = $routeParams.user;
+	$scope.currentUsers = [];
+	$scope.errorMessage = '';
+
+	socket.on('updateUsers', function(roomName, users, ops) {
+		if(!success) {
+			$scope.errorMessage = "ERROR";
+		}
+	});	
 });
