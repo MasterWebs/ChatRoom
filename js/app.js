c3cd3186ad066c2b66feb833edd520bbbbfd0dcc
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-02-15 15:24:12 +0000

    login should be 100% working

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ ChatRoom.config(
 				templateUrl: 'views/login.html', 
 				controller: 'LoginController'
 			})
-			.when('/rooms:user/', {
+			.when('/rooms/:user/', {
 				templateUrl: 'views/rooms.html',
 				controller: 'RoomsController'
+			})
+			.when('rooms/:user/:room/', {
+				templateUrl: 'views/room.html',
+				controller: 'RoomController'
 			})
 			.otherwise({
 				redirectTo: '/login'
@@ -17,14 +21,13 @@ ChatRoom.config(
 	}
 );
 
-ChatRoom.controller('LoginController', function($scope, $location, $rootScope, $routeParams, socket) {
+ChatRoom.controller('LoginController', function ($scope, $location, $rootScope, $routeParams, socket) {
 
 	$scope.errorMessage = '';
 	$scope.nickname = '';
 
 	$scope.login = function() {
-		if ($scope.nickname = '') {
-			console.log("error, empty nick");
+		if ($scope.nickname === '') {
 			$scope.errorMessage = 'Please choose a nickname before continuing!';
 		} else {
 			socket.emit('adduser', $scope.nickname, function (available) {
@@ -38,12 +41,16 @@ ChatRoom.controller('LoginController', function($scope, $location, $rootScope, $
 	};
 });
 
-ChatRoom.controller('RoomsController', function($scope, $location, $rootScope, $routeParams, socket) {
-	$scope.rooms = ['Room1', 'Room2', 'Room3'];
+ChatRoom.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
+	// TODO: Query chat server for active rooms
+	$scope.rooms = [];
+	socket.emit('rooms', function(roomlist) {
+		$scope.rooms = roomlist;
+	});
 	$scope.currentUser = $routeParams.user;
 });
 
-ChatRoom.controller('RoomController', function($scope, $location, $rootScope, $routeParams, socket) {
+ChatRoom.controller('RoomController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
