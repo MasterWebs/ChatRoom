3b87f1132410b7f285c2db46f7edd3767ec6c0ad
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-02-22 14:43:48 +0000

    fixed a minor bug in chatserver.js, now the user disconnects automatically

--- chatserver.js
+++ chatserver.js
@@ -147,7 +147,7 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	// when the user disconnects.. perform this
-	socket.on('log-out', function(){
+	socket.on('disconnect', function(){
 		if(socket.username) {
 			//If the socket doesn't have a username the client joined and parted without
 			//chosing a username, so we just close the socket without any cleanup.
